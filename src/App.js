4aea8e02db631ca394aa865b0c3f1ef1db7fe224
Patryk Orwat <patrykorwat@gmail.com>
2024-11-09 11:13:58 +0100

    display current levels, download spectrum

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Button from '@mui/material/Button';
 import ButtonGroup from '@mui/material/ButtonGroup';
 import clsx from 'clsx';
 import Chip from '@mui/material/Chip';
+import { LineChart, lineElementClasses } from '@mui/x-charts/LineChart';
 
 
 import {
@@ -18,10 +19,20 @@ import { FormControl, useFormControlContext } from '@mui/base/FormControl';
 
 import MenuItem from '@mui/material/MenuItem';
 import Select from '@mui/material/Select';
-
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
 
 import Stack from '@mui/system/Stack';
 import { styled } from '@mui/system';
+
+function downloadFile(fileName, urlData) {
+  var aLink = document.createElement('a');
+  aLink.download = fileName;
+  aLink.href = urlData;
+
+  var event = new MouseEvent('click');
+  aLink.dispatchEvent(event);
+}
 
 const NumberInput = React.forwardRef(function CustomNumberInput(
   props,
@@ -222,6 +233,12 @@ const grey = {
   900: '#1C2025',
 };
 
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
+
 const width = 1024,
 height = 500;
 
@@ -264,6 +281,7 @@ Uint8Array.prototype.indexOfMulti = function(searchElements, fromIndex) {
 const heatmap = ["05000b", "05000b", "0a000a", "0a000a", "0e000f", "0e000f", "130014", "130014", "190018", "190018", "1e001e", "1e001e", "230022", "230022", "290029", "290029", "2e002e", "2e002e", "320033", "320033", "370037", "370037", "3d003d", "3d003d", "450046", "450046", "4e014e", "4e014e", "560157", "560157", "5f005e", "5f005e", "670167", "670167", "6f0070", "6f0070", "780179", "780179", "81017b", "81017b", "870278", "870278", "8d0272", "8d0272", "94036c", "94036c", "9a0467", "9a0467", "a00560", "a00560", "a8065a", "a8065a", "ad0753", "ad0753", "b4084d", "b4084d", "bb0a47", "bb0a47", "c10a40", "c10a40", "c60b38", "c60b38", "cc0c33", "cc0c33", "d30e2c", "d30e2c", "d90d26", "d90d26", "df0f1f", "df0f1f", "e61019", "e61019", "ec1213", "ec1213", "f3120d", "f3120d", "f91306", "f91306", "fc1601", "fc1601", "fe2000", "fe2000", "ff2a01", "ff2a01", "ff3501", "ff3501", "ff3f00", "ff3f00", "ff4b01", "ff4b01", "ff5500", "ff5500", "ff5f01", "ff5f01", "ff6901", "ff6901", "ff7300", "ff7300", "ff7f00", "ff7f00", "ff8901", "ff8901", "ff9400", "ff9400", "ff9f01", "ff9f01", "ffa900", "ffa900", "ffb401", "ffb401", "ffbd02", "ffbd02", "ffc303", "ffc303", "ffc704", "ffc704", "ffcb06", "ffcb06", "ffd009", "ffd009", "ffd30a", "ffd30a", "ffd70d", "ffd70d", "ffdb0d", "ffdb0d", "ffdf10", "ffdf10", "fee310", "fee310", "ffe713", "ffe713", "ffec15", "ffec15", "ffef17", "ffef17", "fff319", "fff319", "fff61b", "fff61b", "fffb1d", "fffb1d", "feff20", "feff20", "f9fb1f", "f9fb1f", "f3f71f", "f3f71f", "eff41f", "eff41f", "eaf01f", "eaf01f", "e5ed20", "e5ed20", "dee920", "dee920", "dae720", "dae720", "d5e320", "d5e320", "cfdf20", "cfdf20", "cbdc21", "cbdc21", "c5d720", "c5d720", "c1d521", "c1d521", "bad020", "bad020", "b5cd21", "b5cd21", "afc920", "afc920", "abc621", "abc621", "a6c220", "a6c220", "a1c021", "a1c021", "9bbc20", "9bbc20", "96b823", "96b823", "93b82a", "93b82a", "91b932", "91b932", "8ebb3c", "8ebb3c", "8bbb43", "8bbb43", "8abc4c", "8abc4c", "88bd54", "88bd54", "85bd5c", "85bd5c", "83be64", "83be64", "80bf6e", "80bf6e", "7dbf75", "7dbf75", "7bc07e", "7bc07e", "7ac187", "7ac187", "78c290", "78c290", "75c298", "75c298", "72c4a1", "72c4a1", "70c5aa", "70c5aa", "6ec4b2", "6ec4b2", "6cc5ba", "6cc5ba", "6ac7c3", "6ac7c3", "6ac6c6", "6ac6c6", "72cbcd", "72cbcd", "7aced1", "7aced1", "83d1d5", "83d1d5", "8ed5d7", "8ed5d7", "97d8da", "97d8da", "a0dcdd", "a0dcdd", "a9dfe1", "a9dfe1", "b3e3e5", "b3e3e5", "bee7e9", "bee7e9", "c6e9eb", "c6e9eb", "d0eef0", "d0eef0", "d9f1f3", "d9f1f3", "e3f5f7", "e3f5f7", "edf9fa", "edf9fa", "f6fcfd", "f6fcfd", "fdffff", "fdffff"],
 moveBy = 1;
 
+let latestPowerPoints = [];
 
 function App() {
   const [portState, setPort] = useState(undefined);
@@ -272,17 +290,40 @@ function App() {
   const [stopFrequency, setStopFrequency] =   useState(108);
   const [stopFrequencyMag, setStopFrequencyMag] =   useState(1000000);
   const [points, setPoints] = useState(500);
+  const [powerLevels, setPowerLevels] = useState([]);
+  const [powerPoints, setPowerPoints] = useState([]);
+
+  useEffect(() => {
+    latestPowerPoints = powerPoints;
+  }, [powerPoints]);
 
   const [currLvl, setCurrLvl] = useState("N/A");
 
-  useEffect(()=>{
-    const canvas = document.getElementById("canvas");
-    var rect = canvas.parentNode.getBoundingClientRect();
-    canvas.width = rect.width;
-    canvas.height = rect.height;
-  }, [])
+  const xPoints = [];
+  const span = (stopFrequency*stopFrequencyMag - startFrequency*startFrequencyMag) / points;
+  for(let i=0;i<points; i++) {
+    xPoints.push(startFrequency*startFrequencyMag + span * i);
+  }
 
   const filters = [tinySAUltra];
+
+  const reset = () => {
+    setPowerLevels([]);
+    setPowerPoints([]);
+
+    const canvas = document.getElementById("canvas"),
+    canvasContext = canvas.getContext("2d");
+    canvasContext.clearRect(0, 0, canvas.width, canvas.height);
+  };
+  const download = () => {
+    console.log(powerPoints);
+    let lines = ''
+    for(let i=0; i<powerPoints.length; i++) {
+      lines += powerPoints[i].join(',');
+      lines += '\n';
+    }
+    downloadFile(`spectrum-${new Date().toISOString()}.csv`, 'data:text/csv;charset=UTF-8,' + encodeURIComponent(lines));
+  };
 
   const drawLines = (powers) => {
     // min 90, mx 10, add case for values higher 
@@ -299,6 +340,7 @@ function App() {
         powerLevels.push(Math.floor(powerVal / bucketSize));
       }
     }
+    setPowerLevels(powers);
     const canvas = document.getElementById("canvas"),
     canvasContext = canvas.getContext("2d");
     canvasContext.drawImage(canvasContext.canvas, 0, 0, width, height - moveBy, 0, moveBy, width, height - moveBy);
@@ -331,6 +373,7 @@ function App() {
     }
 
     drawLines(powers);
+    setPowerPoints([...latestPowerPoints, powers])
   }
 
   let responseBuffer = new Int8Array([]);
@@ -418,12 +461,17 @@ const handleMouseMove = (event) => {
 };
 
 return (
+  <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
   <Container maxWidth="lg">
     <Box display="flex"
       justifyContent="center"
       alignItems="center"
-      minHeight="10vh">
-      <ButtonGroup variant="contained" aria-label="Basic button group" sx={{ marginRight: '30px' }}>
+      minHeight="10vh"
+      sx={{ marginTop: '30px' }}>
+
+      <Stack spacing={2} sx={{ marginRight: '30px' }}>
+        <ButtonGroup variant="contained" aria-label="Basic button group">
         <Button disabled={portState !== undefined} onClick={ async () => {
         port = await navigator.serial.requestPort({ filters });
         const productId = port.getInfo().usbProductId;
@@ -432,9 +480,7 @@ return (
         console.log("connected")
         console.log(port)
 
-        const canvas = document.getElementById("canvas"),
-        canvasContext = canvas.getContext("2d");
-        canvasContext.clearRect(0, 0, canvas.width, canvas.height);
+        reset();
 
         readLoop(port);
         writeLoop(port);
@@ -456,7 +502,10 @@ return (
         }
       }
       }}>Disconnect</Button>
-    </ButtonGroup>
+        </ButtonGroup>
+        <Button onClick={reset}>Reset</Button>
+        <Button onClick={download}>Download spectrum</Button>
+      </Stack>
 
     <FormControl defaultValue="" >
       <Label>Points</Label>
@@ -470,7 +519,7 @@ return (
         onChange={(event, val) => setPoints(val)}
       />
     </FormControl>
-    <FormControl defaultValue="" sx={{ marginRight: '15px' }}>
+    <FormControl defaultValue="">
       <Label>Start frequency [Hz]</Label>
       <Stack direction="row" >
         <NumberInput
@@ -484,6 +533,7 @@ return (
           disabled={portState !== undefined}
           value={startFrequencyMag}
           onChange={(event) => setStartFrequencyMag(event.target.value)}
+          sx={{ marginRight: '15px' }}
         >
           <MenuItem value={1}>Hz</MenuItem>
           <MenuItem value={1000}>kHz</MenuItem>
@@ -517,12 +567,23 @@ return (
     </FormControl>
     
   </Box>
-  <Stack direction="row" spacing={1}>
-      <Chip label={`Current Frequency: ${currLvl}`} variant="outlined" />
-    </Stack>
   <Box
     display='flex'
     justifyContent='center'
+  >
+    <LineChart
+      width={width+80}
+      height={300}
+      slotProps={{ legend: { hidden: true } }}
+      series={[{ data: powerLevels, label: 'dB', area: true, showMark: false, color: '#cc0052' }]}
+      xAxis={[{ scaleType: 'linear', data: xPoints }]}
+      sx={{ }}
+    />
+  </Box>
+  <Box
+    display='flex'
+    justifyContent='center'
+    sx={{ paddingLeft: '30px' }}
   >
     <canvas
       id="canvas" 
@@ -530,9 +591,15 @@ return (
       height={height}
       onMouseMove={handleMouseMove}
       onTouchMove={handleMouseMove}
+      onMouseOut={()=> setCurrLvl("N/A")}
+      onTouchEnd={()=> setCurrLvl("N/A")}
       ></canvas>
   </Box>
+  <Stack direction="row" spacing={1}>
+      <Chip label={`Current Frequency: ${currLvl}`} variant="outlined" />
+  </Stack>
 </Container>
+</ThemeProvider>
 );
 }
 
